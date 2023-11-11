0b72db009f3e4e24c73e9cdd183c1b95f6dbbca1
David Velázquez Morales <101413618+DaytonPlus@users.noreply.github.com>
2023-11-11 17:32:49 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,10 @@ export default {
   title: "Horario DevClient",
   host: "0.0.0.0",
   port: 8000,
+  backend: {
+    host: "0.0.0.0",
+    port: 8001
+  },
   logLevel: 1,
   firstBuild: true,
   devMode: false,
