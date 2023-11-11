a3b1576c3a35cd4eb4419c08cca9ad00201014d1
David Velázquez Morales <101413618+DaytonPlus@users.noreply.github.com>
2023-11-11 16:49:22 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+export default {
+  title: "Horario DevClient",
+  host: "0.0.0.0",
+  port: 8000,
+  logLevel: 1,
+  firstBuild: false,
+  devMode: false,
+  offline: false,
+  stylesDir: "styles",
+  buildsDir: "public/css",
+}
